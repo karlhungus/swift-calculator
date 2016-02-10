4432483b1732a8372c84dc557699a0b3e1e71967
Izaak Alpert <izaak.alpert@shopify.com>
2016-02-10 07:18:58 -0500

    smooth operators

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,20 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	@IBOutlet weak var display: UILabel!
-	var userIsInTheMiddleOfTypingANumber: Bool = false
-
-	@IBAction func appendDigit(sender: UIButton) {
-		let digit = sender.currentTitle!
-		if userIsInTheMiddleOfTypingANumber {
-			display.text = display.text! + digit
-		}
-		else {
-			userIsInTheMiddleOfTypingANumber = true
-			display.text = digit			
-		}
-		print("digit = \(digit)")
-	}
+  
+  @IBOutlet weak var display: UILabel!
+  var userIsInTheMiddleOfTypingANumber: Bool = false
+  
+  @IBAction func appendDigit(sender: UIButton) {
+    let digit = sender.currentTitle!
+    if userIsInTheMiddleOfTypingANumber {
+      display.text = display.text! + digit
+    }
+    else {
+      userIsInTheMiddleOfTypingANumber = true
+      display.text = digit			
+    }
+  }
+  @IBAction func operate(sender: UIButton) {
+    let operation = sender.currentTitle!
+    if userIsInTheMiddleOfTypingANumber {
+      enter()
+    }
+    switch operation {
+    case "×":
+      if operandStack.count >= 2 {
+        displayValue = operandStack.removeLast() * operandStack.removeLast()
+        enter()
+      }
+      //case "÷":
+      //case "+":
+      //case "-":
+    default: break;
+    }
+  }
+  
+  var operandStack = Array<Double>()
+  
+  @IBAction func enter() {
+    userIsInTheMiddleOfTypingANumber = false
+    operandStack.append(displayValue)
+    print("operandStack: \(operandStack)")
+  }
+  
+  var displayValue : Double {
+    get {
+      return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+    }
+    set {
+      display.text = "\(newValue)"
+      userIsInTheMiddleOfTypingANumber = false
+    }
+  }
+  
 }
 
